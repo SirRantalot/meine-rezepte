df4c381e2e0d11a02aa0581ef3bccf07d652f907
SirRantalot <richard.hargrave@bbzbl-it.ch>
2023-11-17 11:30:14 +0100

    Geolocater

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { DayNightComponent } from '../day-night/day-night.component';
+import { BatteryComponent } from '../battery/battery.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, DayNightComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, DayNightComponent, BatteryComponent],
 })
 export class HomePage {
   constructor() {}
