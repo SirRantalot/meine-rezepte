b06893cbb78076f5850c5c00b6a54d7167768770
SirRantalot <richard.hargrave@bbzbl-it.ch>
2023-11-24 10:46:24 +0100

    Fixed env issues

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   supabaseUrl: 'https://kluqjlbqjczynohqygqr.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtsdXFqbGJxamN6eW5vaHF5Z3FyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA3MzA3NjEsImV4cCI6MjAxNjMwNjc2MX0.scbxovU6Nyzsh_e0mA_kvglDcsa9I4qhV0cwIX-PrWQ'
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtsdXFqbGJxamN6eW5vaHF5Z3FyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMDczMDc2MSwiZXhwIjoyMDE2MzA2NzYxfQ.gw19Cgo2cywd6pBuCoqqfgbQ0vS0M5Yh_cXoLxBJGi4'
 };
